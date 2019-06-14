7b98ff84ec5823ebeafb9578d968b19006b7e875
Jiomod <lymickael91@gmail.com>
2019-06-14 17:22:38 +0200

    up

---
+++ grandpy/demo.py
@@ -0,0 +1,5 @@
+# def g():
+#     return "fonctionne"
+
+# def f():
+#     return g()
---
+++ tests/test_demo.py
@@ -0,0 +1,13 @@
+# from grandpy.demo import f,g
+# from grandpy import demo
+
+# def test_ma_fonction_f_est_ok(monkeypatch):
+#     def mock_g():
+#         return "tout fonctionne"
+#     monkeypatch.setattr('grandpy.demo.g',mock_g)
+#     result = f()
+#     assert result == "tout fonctionne"
+
+# def test_ma_fonction_g_se_comporte_correctement():
+#     result = g()
+#     assert result == "tout fonctionne"
