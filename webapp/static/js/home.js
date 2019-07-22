c16c219c831afdc722d77ab78aa98c5a3a3e0e99
Jiomod <lymickael91@gmail.com>
2019-07-22 22:02:20 +0200

    up

--- webapp/static/js/home.js
+++ webapp/static/js/home.js
@@ -1,8 +1,19 @@
+// Initialize and add the map
+function initMap() {
+    // The location of Uluru
+    var uluru = {lat: -25.344, lng: 131.036};
+    // The map, centered at Uluru
+    var map = new google.maps.Map(
+        document.getElementById('map'), {zoom: 4, center: uluru});
+    // The marker, positioned at Uluru
+    var marker = new google.maps.Marker({position: uluru, map: map});
+  }
+
 function afficher(response) {
-    console.log(response);
+    console.log(JSON.stringify(response));
 }
 
-ajaxGet("http://127.0.0.1:5000", afficher);
+// ajaxGet("http://127.0.0.1:5000", afficher);
 
 form.addEventListener('submit', function(e){
     //remove for the view display
@@ -10,6 +21,7 @@ form.addEventListener('submit', function(e){
     var loader = document.getElementById( "loader" )
     var inputext = document.getElementById('inputext').value;
     var newchat = document.getElementById('chat');
+    var gmap = document.getElementById('map');
     if(inputext === '') {
         newchat.textContent = 'PAS DE QUESTION RENSEIGNEE';
     }
@@ -17,9 +29,9 @@ form.addEventListener('submit', function(e){
         loader.style.display = "block";
         setTimeout( function() {loader.style.display = "none"}, 3000);
         setTimeout(function() {newchat.textContent = inputext},3000);
-        ajaxGet("http://127.0.0.1:5000/_api", afficher)
+        setTimeout(function() {gmap.style.display = 'block'}, 3000);
     }
-    
+    // ajaxGet("http://127.0.0.1:5000/_api", afficher)
     
 })
 
