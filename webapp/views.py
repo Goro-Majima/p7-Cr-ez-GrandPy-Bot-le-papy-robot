e348cec220f1f8e4dad6c36f2c0044bc853f0328
Jiomod <lymickael91@gmail.com>
2019-07-30 16:03:27 +0200

    cors

--- webapp/views.py
+++ webapp/views.py
@@ -8,7 +8,7 @@ from grandpy.mainfile import process_question
 app = Flask(__name__)
 CORS(app)
 app.config['JSON_AS_ASCII'] = False # keep tones when returning json file
-# app.config['CORS_HEADERS'] = 'Content-Type'
+app.config['CORS_HEADERS'] = 'Content-Type'
 
 @app.route('/')
 @app.route('/home/')
@@ -17,7 +17,7 @@ def home():
     return render_template('home.html')
 
 @app.route('/_api', methods=['POST'])
-# @cross_origin(origin='http://127.0.0.1:5000/_api',headers=['Content-Type','Authorization'])
+@cross_origin(origin='http://127.0.0.1:5000/_api',headers=['Content-Type','Authorization'])
 def api_response():
     """send response to the server at url _api"""
     text = request.form['usertext']
