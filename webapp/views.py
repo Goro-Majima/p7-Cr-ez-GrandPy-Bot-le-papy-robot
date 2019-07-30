69c03c5953386de4cbb372bc24b9b1cd40255f6f
Jiomod <lymickael91@gmail.com>
2019-07-30 14:49:25 +0200

    fix html

--- webapp/views.py
+++ webapp/views.py
@@ -1,12 +1,13 @@
 """file used according to the mvt, send output to the route"""
 from flask import Flask, render_template, jsonify, request
-# from flask_cors import CORS, cross_origin
+from flask_cors import CORS, cross_origin
 
 from grandpy.mainfile import process_question
 
 
 app = Flask(__name__)
 app.config['JSON_AS_ASCII'] = False # keep tones when returning json file
+app.config['CORS_HEADERS'] = 'Content-Type'
 
 @app.route('/')
 @app.route('/home/')
@@ -14,20 +15,24 @@ def home():
     """display homepage to the url /home/"""
     return render_template('home.html')
 
-# @app.route('/_api', methods=['POST'])
-# # @cross_origin(origin='http://127.0.0.1:5000/_api',headers=['Content-Type','Authorization'])
-# def api_response():
-#     """send response to the server at url _api"""
-#     text = request.form['usertext']
-#     response = process_question(text)
-#         # return render_template('momo.html',text=response)
-#     return jsonify(response)
-
-@app.route('/_api/', methods= ['GET'])
+@app.route('/_api', methods=['POST'])
+# @cross_origin(origin='http://127.0.0.1:5000/_api',headers=['Content-Type','Authorization'])
 def api_response():
-    text = request.args.get('usertext','disney')
+    """send response to the server at url _api"""
+    text = request.form['usertext']
     response = process_question(text)
-    return jsonify(response)
+        # return render_template('momo.html',text=response)
+    responsecors =jsonify(response)
+    responsecors.headers.add("Access-Control-Allow-Origin", "*")
+    return responsecors
+
+# @app.route('/_api/', methods= ['GET'])
+# def api_response():
+#     text = request.args.get('usertext')
+#     print(text)
+#     print(6)
+#     response = process_question(text)
+#     return jsonify(response)
 
 if __name__ == "__main__":
     app.run(debug=True, port=5000)
