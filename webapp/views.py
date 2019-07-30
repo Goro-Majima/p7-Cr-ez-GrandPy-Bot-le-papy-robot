b84ef728f423361d2fffb1d0c1b545d1e2866d22
Jiomod <lymickael91@gmail.com>
2019-07-30 14:39:27 +0200

    fix html

--- webapp/views.py
+++ webapp/views.py
@@ -14,22 +14,20 @@ def home():
     """display homepage to the url /home/"""
     return render_template('home.html')
 
-@app.route('/_api', methods=['POST'])
-# @cross_origin(origin='http://127.0.0.1:5000/_api',headers=['Content-Type','Authorization'])
-def api_response():
-    """send response to the server at url _api"""
-    text = request.form['usertext']
-    response = process_question(text)
-        # return render_template('momo.html',text=response)
-    return jsonify(response)
-
-# @app.route('/_api/', methods= ['GET'])
+# @app.route('/_api', methods=['POST'])
+# # @cross_origin(origin='http://127.0.0.1:5000/_api',headers=['Content-Type','Authorization'])
 # def api_response():
-#     text = request.args.get('usertext')
-#     print(text)
-#     print(6)
+#     """send response to the server at url _api"""
+#     text = request.form['usertext']
 #     response = process_question(text)
+#         # return render_template('momo.html',text=response)
 #     return jsonify(response)
+
+@app.route('/_api/', methods= ['GET'])
+def api_response():
+    text = request.args.get('usertext','disney')
+    response = process_question(text)
+    return jsonify(response)
 
 if __name__ == "__main__":
     app.run(debug=True, port=5000)
