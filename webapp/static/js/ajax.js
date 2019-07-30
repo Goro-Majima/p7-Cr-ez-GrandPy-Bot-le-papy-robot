e0fbdb45383f9f4faec658b7910551110276c15a
Jiomod <lymickael91@gmail.com>
2019-07-30 14:12:04 +0200

    fix html

--- webapp/static/js/ajax.js
+++ webapp/static/js/ajax.js
@@ -21,8 +21,9 @@ function ajaxGet(url, callback) {
 // Prend en paramètres l'URL cible, la donnée à envoyer et la fonction callback appelée en cas de succès
 function ajaxPost(url, data, callback) {
     var req = new XMLHttpRequest();
-    req.open("POST", url);
     req.setRequestHeader('Access-Control-Allow-Origin','*');
+    req.setRequestHeader('X-Content-Type-Options', 'nosniff');
+    req.open("POST", url);
     req.addEventListener("load", function () {
         if (req.status >= 200 && req.status < 400) {
             // Appelle la fonction callback en lui passant la réponse de la requête
